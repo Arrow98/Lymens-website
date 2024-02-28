ce959ae3c1ff64a99286db32911f1a7477f2e88f
Michael Gyamfi <mg180802@mail.com>
2024-02-28 11:58:41 +0000

    updated

--- script.js
+++ script.js
@@ -1,17 +1,29 @@
-document.addEventListener("DOMContentLoaded", function() {
-    var boxes = document.querySelectorAll('.box');
-  
-    function showBoxes() {
-      boxes.forEach(function(box) {
-        var boxTop = box.getBoundingClientRect().top;
-        var windowHeight = window.innerHeight;
-        if (boxTop < windowHeight) {
-          box.classList.add('visible');
-        }
-      });
-    }
-  
-    window.addEventListener('scroll', showBoxes);
-    showBoxes();
-  });
-  
+const button = document.querySelectorAll(".button")
+const overLay = document.getElementById("overlay")
+const cancelIcon = document.getElementById("cancel-icon")
+const apply = document.getElementById("apply")
+const form = document.getElementById("application-form")
+
+
+  for(let i = 0; i < button.length; i++){
+    button[i].addEventListener('click', function() {
+
+    window.location.href = '/pages/aboutus.html';
+    });
+  }
+
+
+apply.addEventListener('click', function(){
+  overLay.classList.remove('hidden')
+  form.classList.remove('hidden')
+})
+   
+cancelIcon.addEventListener('click',function(){
+  overLay.classList.add('hidden')
+  form.classList.add('hidden')
+})
+
+overLay.addEventListener('click',function(){
+  overLay.classList.add('hidden')
+  form.classList.add('hidden')
+})
