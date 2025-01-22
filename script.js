e33a7725fea2a2636767f1e3a9a2ebd806dee67d
Arrow98 <gm180802@gmail.com>
2025-01-21 17:39:23 -0800

    dassssh

--- script.js
+++ script.js
@@ -1,39 +1,36 @@
-const button = document.querySelectorAll(".button")
-const overLay = document.getElementById("overlay")
-const cancelIcon = document.getElementById("cancel-icon")
-const apply = document.getElementById("apply")
-const form = document.getElementById("application-form")
+const button = document.querySelectorAll(".button");
+const overLay = document.getElementById("overlay");
+const cancelIcon = document.getElementById("cancel-icon");
+const apply = document.getElementById("apply");
+const form = document.getElementById("application-form");
 
-
-  for(let i = 0; i < button.length; i++){
-    button[i].addEventListener('click', function() {
-
-    window.location.href = '/pages/aboutus.html';
-    });
-  }
-  function hidden(){
-  overLay.classList.remove('hidden')
-  form.classList.remove('hidden')
-  }
-
-function add (){
-  overLay.classList.add('hidden')
-  form.classList.add('hidden')
+for (let i = 0; i < button.length; i++) {
+  button[i].addEventListener("click", function () {
+    window.location.href = "/pages/aboutus.html";
+  });
 }
-apply.addEventListener('click', function(){
-  hidden()
-})
-   
-cancelIcon.addEventListener('click',function(){
-  add()
-})
+function hidden() {
+  overLay.classList.remove("hidden");
+  form.classList.remove("hidden");
+}
 
-overLay.addEventListener('click',function(){
-  add()
-})
+function add() {
+  overLay.classList.add("hidden");
+  form.classList.add("hidden");
+}
+apply.addEventListener("click", function () {
+  hidden();
+});
+
+cancelIcon.addEventListener("click", function () {
+  add();
+});
+
+overLay.addEventListener("click", function () {
+  add();
+});
 
 document.getElementById("application-form").addEventListener("click", (e) => {
-  e.stopPropagation()
- 
-} )
-
+  e.stopPropagation();
+});
+// dashhhhhhh
